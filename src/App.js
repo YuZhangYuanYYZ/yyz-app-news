dc32fc464c37a6e4c3c084a8cdd01ea45d5bf5eb
Jing Liu <jing.liu@rea-group.com>
2019-05-08 09:12:33 +1000

    how to import images from local

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 import NewsContainer from './NewsContainer';
+import testPhoto from './images/test.png'
+
+function importAll(r) {
+  return r.keys().map(r);
+}
+const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       </header>
+      {/* firt way */}
+      <img src={images[0]} alt='test'/>
+       {/* second way */}
+      <img src={testPhoto} alt='test'/>
       <NewsContainer />
     </div>
   );
